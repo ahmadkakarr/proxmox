e485a3f02672c711c59c0e72a5863c9493756fab
Ahmad Hassan Khan <ahmadkakarr@gmail.com>
2024-07-09 16:08:22 +0500

    update

--- lxc/modules/ct-managemet/create-lxc-containers.sh
+++ lxc/modules/ct-managemet/create-lxc-containers.sh
@@ -7,7 +7,6 @@ declare -A containers_description
 declare -A containers_tags
 declare -A users_and_passwords
 
-php8-fpm-1="the description for php-fpm";
 
 
 containers_array=( 
@@ -100,7 +99,6 @@ pct create $id /var/lib/vz/template/cache/ubuntu-24.04-standard_24.04-2_amd64.ta
     --nameserver $nameserver \
     --searchdomain $searchdomain \
     --start 1 \
-    # --tags $tags \
     --features nesting=1,keyctl=1 \
     --net0 name=eth0,bridge=vmbr0,ip=dhcp,type=veth  &&\
 echo "starting container"
